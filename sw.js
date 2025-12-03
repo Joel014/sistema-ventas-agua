1b0019527eab6520c950360fa1e38aeff84070e4
Joel014 <joelrs.14@hotmail.com>
2025-12-03 15:45:42 -0400

    Remove application logo and restore index.html

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'awa-v10';
+const CACHE_NAME = 'awa-v12';
 const ASSETS = [
     './',
     './index.html',
-    './css/style.css',
+    './css/main.css',
     './js/script.js',
     './js/auth.js',
     './js/firebase-config.js',
